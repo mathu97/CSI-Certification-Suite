ac7ef66219468ff2c41957481fc0972c5a28d396
Mathusan Selvarajah <mselvara@redhat.com>
2018-11-21 10:55:45 -0500

    changing shell script to more appropriate name

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -15,7 +15,12 @@ printf "\nSSH key generated for GCE\n\n"
 #Stand Up a Cluster
 (cd $KUBE_PATH && go run hack/e2e.go -- --provider=gce --gcp-nodes=1 --gcp-network=mselvara-e2e --gcp-project=openshift-gce-devel --gcp-zone=us-east1-d --up)
 
-#Add bootstrapping and e2e test part here   
+#Deploy Bootstrap
+go build e2e-bootstrap.go
+docker build -f DeployBootstrap/Dockerfile -t mselvara/e2e-bootstrap .
+kubectl create -f DeployBootstrap/bootstrapPod.yaml
+
+#Add e2e part here
 
 #Bring Cluster Down
 #(cd $KUBE_PATH && go run hack/e2e.go -- --provider=gce --gcp-nodes=1 --gcp-network=mselvara-e2e --gcp-project=openshift-gce-devel --gcp-zone=us-east1-d --down)
